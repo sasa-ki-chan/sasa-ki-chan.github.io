fbf0c72e7449f302967a6381bf56a61b75331be4
sasa <udongunma@gmail.com>
2023-10-08 20:08:49 +0900

    調整

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-// クリック要素を取得
-const target = document.getElementById('sasaki');
+const sasaki = document.querySelector('.sasaki');
 
-// クリックイベントリスナーを追加
-target.addEventListener('click', function () {
-    // clickedクラスを切り替えて縮小アニメーションを開始
-    target.classList.toggle('clicked');
-
-    // 一定時間が経過したらclickedクラスを削除して元に戻す
+sasaki.addEventListener('click', () => {
+    sasaki.classList.toggle('scale');
     setTimeout(function () {
-        target.classList.remove('clicked');
-    }, 1000); // 1000ミリ秒（1秒）後に元に戻す
+        sasaki.classList.remove('scale');
+    }, 300);
 });
