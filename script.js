6959f3de7d47de41274e1785996059ba2ef252bd
sasa <udongunma@gmail.com>
2023-10-08 17:56:38 +0900

    機能追加

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
-// ページ内のすべての要素を取得
-const elements = document.querySelectorAll('*');
+// クリック要素を取得
+const target = document.getElementById('sasaki-chibi');
 
-// 有効なクラスとクエリを格納する配列
-const validStyles = [];
+// クリックイベントリスナーを追加
+target.addEventListener('click', function () {
+    // clickedクラスを切り替えて縮小アニメーションを開始
+    target.classList.toggle('clicked');
 
-// 各要素についてクラスとクエリを確認
-elements.forEach((element) => {
-    const computedStyle = getComputedStyle(element);
-    const classes = element.classList;
-
-    // クラスの確認
-    classes.forEach((className) => {
-        if (computedStyle.getPropertyValue('color') === 'rgb(255, 0, 0)' &&
-            computedStyle.getPropertyValue('font-size') === '24px') {
-            validStyles.push(`クラス "${className}" が適用されています。`);
-        }
-    });
-
-    // クエリの確認
-    if (computedStyle.getPropertyValue('border-top-color') === 'rgb(0, 0, 255)') {
-        validStyles.push('メディアクエリ内のスタイルが適用されています。');
-    }
+    // 一定時間が経過したらclickedクラスを削除して元に戻す
+    setTimeout(function () {
+        target.classList.remove('clicked');
+    }, 1000); // 1000ミリ秒（1秒）後に元に戻す
 });
-
-// 結果を表示
-const resultElement = document.getElementById('result');
-if (validStyles.length > 0) {
-    resultElement.textContent = '以下のスタイルが有効になっています：\n\n' + validStyles.join('\n');
-} else {
-    resultElement.textContent = '有効なスタイルは見つかりませんでした。';
-}
